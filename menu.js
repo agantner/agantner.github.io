4046c868e2dab231e3cd14c67f43fe575ad56a05
agantner <anita.gantner@uibk.ac.at>
2024-12-13 14:15:48 +0100

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ document.write(' <div id="eeecon-col1">                 					');
 document.write(' <div id="menu">                                                                ');
 document.write('   <ul class="menu">                                   			        ');
 document.write('   <li><a href="index.html"><span>Home</span></a></li>   		        ');
-document.write('   <li><a href="curriculumvitae22.html"><span>CV</span></a></li>                  ');
+document.write('   <li><a href="curriculumvitae22.html"><span>CV</span></a></li>                ');
 document.write('   <li><a href="publications.html"><span>Publications</span></a></li>           ');
 document.write('   <li><a href="teaching.html"><span>Teaching</span></a></li>                   ');
 document.write('   </ul>                                                                        ');
