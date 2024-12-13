5c9044b929dc58da6fa9bcd6680358fc96dc4eca
agantner <anita.gantner@uibk.ac.at>
2024-12-13 14:00:54 +0100

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,13 @@
+document.write(' <div id="eeecon-col1">                 					');
+document.write(' <div id="menu">                                                                ');
+document.write('   <ul class="menu">                                   			        ');
+document.write('   <li><a href="index.html"><span>Home</span></a></li>   		        ');
+document.write('   <li><a href="curriculumvitae.html"><span>CV</span></a></li>                  ');
+document.write('   <li><a href="publications.html"><span>Publications</span></a></li>           ');
+document.write('   <li><a href="teaching.html"><span>Teaching</span></a></li>                   ');
+document.write('   </ul>                                                                        ');
+document.write(' </div>                                                                         ');
+document.write(' </div>                                                                         ');
+
+
+
